a483334052849f83018b197477dc101821a4fbae
arilindra21 <arilindra21@gmail.com>
2024-09-25 10:51:08 +0700

    add files

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -95,15 +95,15 @@ const Contact = () => {
       <style jsx>{`
         .glow-text {
            color: #fff;
-  text-shadow:
-    0 0 5px #fff,
-    0 0 10px #fff,
-    0 0 20px #fff,
-    0 0 40px #0ff,
-    0 0 80px #0ff,
-    0 0 90px #0ff,
-    0 0 100px #0ff,
-    0 0 150px #0ff;
+            text-shadow:
+                0 0 5px #fff,
+                0 0 10px #fff,
+                0 0 20px #fff,
+                0 0 40px #0ff,
+                0 0 80px #0ff,
+                0 0 90px #0ff,
+                0 0 100px #0ff,
+                0 0 150px #0ff;
         }
       `}</style>
     </section>
